2de08cf8208df99b2533315f7a181e0476fc9830
daniel <dennyparas28@gmail.com>
2018-09-19 15:57:01 +0800

    add services route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const aboutRouter = require('./routes/about');
+const servicesRouter = require('./routes/services');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/about', aboutRouter);
+app.use('/services', servicesRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ routes/services.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('services', {
+    pageTitle: 'Services',
+  });
+});
+
+module.exports = router;
